09cd159c9cdecc6529add4635d1f276597f26064
jlechido557370 <j.lechido.557370@umindanao.edu.ph>
2026-04-28 00:08:43 +0800

    help me

--- render-build.sh
+++ render-build.sh
@@ -3,6 +3,7 @@ set -e
 
 composer install --no-dev --optimize-autoloader
 php artisan key:generate --force
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
